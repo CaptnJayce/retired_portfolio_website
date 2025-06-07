71a52b991691c269d9906baebc8376af0ffa3044
captnjayce <captn.dev@protonmail.com>
2025-06-07 19:17:56 +0100

    fixed tooltip

--- src/components/celestial_bodies/hourglass_twins.js
+++ src/components/celestial_bodies/hourglass_twins.js
@@ -36,42 +36,98 @@ export class HourglassTwins extends THREE.Group {
         this.angle = Math.random() * Math.PI * 2;
 
         this.isClickable = true;
+        this.isZoomed = false;
         this.camera = camera;
         this.handleClick = this.onClick.bind(this);
 
         // this.twin1.name = "Ember Twin";
         this.twin1.name = "Experience";
         this.twin1.isHoverable = true;
+        this.twin1.tooltip = this.createTooltip(this.twin1.name);
+        this.twin1.updateTooltip = this.createUpdateTooltipFunction(this.twin1);
         this.twin1.handleMouseOver = () => {
             this.outlineMeshOne.visible = true;
+            this.twin1.tooltip.style.visibility = 'visible';
+            this.twin1.updateTooltip();
         };
         this.twin1.handleMouseOut = () => {
             this.outlineMeshOne.visible = false;
+            this.twin1.tooltip.style.visibility = 'hidden';
         };
 
         // this.twin2.name = "Ash Twin";
-        this.twin2.name = "Education"
+        this.twin2.name = "Education";
         this.twin2.isHoverable = true;
+        this.twin2.tooltip = this.createTooltip(this.twin2.name);
+        this.twin2.updateTooltip = this.createUpdateTooltipFunction(this.twin2);
         this.twin2.handleMouseOver = () => {
             this.outlineMeshTwo.visible = true;
+            this.twin2.tooltip.style.visibility = 'visible';
+            this.twin2.updateTooltip();
         };
         this.twin2.handleMouseOut = () => {
             this.outlineMeshTwo.visible = false;
+            this.twin2.tooltip.style.visibility = 'hidden';
         };
     }
 
+    createTooltip(text) {
+        const tooltip = document.createElement('div');
+        tooltip.style.position = 'fixed';
+        tooltip.style.color = 'white';
+        tooltip.style.fontFamily = 'Arial, sans-serif';
+        tooltip.style.fontSize = '20px';
+        tooltip.style.visibility = 'hidden';
+        tooltip.style.transform = 'translate(-50%, -100%)';
+        tooltip.style.pointerEvents = 'none';
+        tooltip.style.zIndex = '100';
+        tooltip.textContent = text;
+        document.body.appendChild(tooltip);
+        return tooltip;
+    }
+
+    createUpdateTooltipFunction(twin) {
+        return () => {
+            const worldPos = new THREE.Vector3();
+            twin.getWorldPosition(worldPos);
+            worldPos.y += twin.geometry.parameters.radius * 1.5;
+            worldPos.project(this.camera);
+
+            const x = (worldPos.x * 0.5 + 0.5) * window.innerWidth;
+            const y = (1 - (worldPos.y * 0.5 + 0.5)) * window.innerHeight;
+
+            twin.tooltip.style.left = `${x}px`;
+            twin.tooltip.style.top = `${y}px`;
+        };
+    }
+
+
     onClick() {
-        if (this.camera && typeof this.camera.focusOnObject === 'function') {
-            this.twin1.isHoverable = false;
-            this.twin2.isHoverable = false;
+        if (this.isZoomed) {
+            this.camera.resetView();
+            // this.hidePlanetInfo();
+            this.isZoomed = false;
+        } else {
+            if (this.camera && typeof this.camera.focusOnObject === 'function') {
+                this.twin1.isHoverable = false;
+                this.twin2.isHoverable = false;
 
-            this.outlineMeshOne.visible = false;
-            this.outlineMeshTwo.visible = false;
+                this.outlineMeshOne.visible = false;
+                this.outlineMeshTwo.visible = false;
 
-            this.camera.focusOnObject(this, {
-                distance: 10,
-                zoom: 5
-            });
+                this.twin1.tooltip.style.visibility = 'hidden';
+                this.twin2.tooltip.style.visibility = 'hidden';
+
+                this.camera.focusOnObject(this, {
+                    distance: 10,
+                    zoom: 5
+                });
+
+                setTimeout(() => {
+                    this.showPlanetInfo();
+                }, 1000);
+            }
+            this.isZoomed = true;
         }
     }
 
@@ -91,5 +147,8 @@ export class HourglassTwins extends THREE.Group {
 
         this.twin2.position.set(twin2X, twin2Y, 0);
         this.outlineMeshTwo.position.set(twin2X, twin2Y, 0);
+
+        if (this.twin1.tooltip.style.visibility == 'visible') this.twin1.updateTooltip();
+        if (this.twin2.tooltip.style.visibility == 'visible') this.twin2.updateTooltip();
     }
 }
