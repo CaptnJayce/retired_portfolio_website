b82d0bf8f0051c83227f7ceceb2e78a17f537bdb
captnjayce <captn.dev@protonmail.com>
2025-06-23 19:44:48 +0100

    final changes to General Info section

--- src/components/celestial_bodies/sun.js
+++ src/components/celestial_bodies/sun.js
@@ -28,26 +28,46 @@ export class Sun extends BasePlanet {
         `;
 
         main.innerHTML = `
-        <h2>General</h2>
-        <p>Welcome to my website! My name is Casey Jestico, a 22-year-old developer from London</p>
-        <p>I have been making games and websites for almost five years now and have had an interest in technology from as early as I can remember</p>
-        <ul style="font-size: 0.8vw">
-          <li> I have two cats called Willow and Winston</li>
-          <li> My top five favourite games are: Terraria, Outer Wilds, Hollow Knight, Risk of Rain 2, and V Rising</li>
-          <li> My favourite drink is Monster Energy Mango Loco (consumed in moderation)</li>
+        <h2 id="titles">Hello! My name is Casey Jestico</h2>
+        <p style="margin-top: 0;">I'm a web/game developer from London</p>
+        <p>I have been making games and websites for almost five years and have had an interest in technology from as early as I can remember</p>
+        <p>I love to see my projects evolve as I write more code and I get this experience the most with game/web development. I find them to be the best mediums to express myself and my ideas in</p>
+
+        <h2 id="titles">Some fun facts</h2>
+        <p style="margin: 0;">Hobbies</p>
+        <ul style="font-size: 0.8vw; margin: 0;">
+            <li>Gaming</li>
+            <li>3D Printing</li>
+            <li>Creative writing</li>
+            <li>Pixel art</li>
         </ul>
-        <br></br>
-        <h2>FAQ</h2>
-        <p style="font-weight: bold"><i class="nf nf-fa-question" style="color: red"></i> Why are you called Captn-Jayce- if your name is Casey?</p>
-        <p><i class="nf nf-oct-light_bulb" style="color: yellow"></i> Online, I wanted to be called an actual human name instead of a user ID - but I didn't want my real name to be used everywhere, so I picked Jayce by reversing my initials (CJ -> JC) and picking the closest name I could think of.</p>
-        <p>The 'Captn' prefix has no reasoning, I just thought it looked cool and it wasn't taken anywhere.</p>
+
+        <p id="titles">General</p>
+        <ul style="font-size: 0.8vw; margin: 0;">
+            <li>I have two cats called Willow and Winston</li>
+            <li>I know Fares</li>
+            <li>I know what you did, Daniel.</li>
+            <li>My favourite drink is Monster Energy Mango Loco (consumed in moderation)</li>
+        </ul>
+        <p id="titles">Top 5</p>
+        <ul style="font-size: 0.8vw; margin: 0;">
+            <li>Terraria</li>
+            <li>Outer Wilds</li>
+            <li>Hollow Knight</li>
+            <li>Risk of Rain 2</li>
+            <li>V Rising</li>
+        </ul>
+
+        <h2 id="titles">FAQ</h2>
+        <p style="font-weight: bold; margin-top: 0;"><i class="nf nf-fa-question" style="color: red"></i> Why are you called Captn-Jayce- if your name is Casey?</p>
+        <p><i class="nf nf-oct-light_bulb" style="color: yellow"></i> I wanted my online username to have an actual human name but was hesistant to use my real one, so I picked Jayce by reversing my initials (CJ -> JC) and picking the closest name I could think of.</p>
         <p style="font-weight: bold"><i class="nf nf-fa-question" style="color: red"></i> Why did you make an Outer Wilds themed website?</p>
-        <p><i class="nf nf-oct-light_bulb" style="color: yellow"></i> Outer Wilds is one of the greatest gaming experiences I've ever had - and I like to think that says a lot given the fact I've been playing games for around 15 years now. Because of this, I wanted to express my awe for the game by creating something in its image, which just so happened to be a portfolio website.</p>
-        <p>Originally, I was going to make a Terraria themed website, however, I decided against it as I thought the design of Terraria is a little too niche for people who aren't in the gaming sphere. </p>
+        <p><i class="nf nf-oct-light_bulb" style="color: yellow"></i> I really like Outer Wilds and thought it'd be a neat idea</p>
+        <p><i class="nf nf-oct-light_bulb" style="color: yellow"></i><i class="nf nf-oct-light_bulb" style="color: yellow"></i> Originally, I was going to make a Terraria themed website, however, I decided against it as I thought the design of Terraria is a little too niche for people who aren't in the gaming sphere. </p>
         `;
 
         tools.innerHTML = `
-        <h2>What I use</h2>
+        <h2 id="titles">What I use</h2>
 
         <!-- thank you deepseek for the pixel to vw/vh conversions -->
         <div style="display: flex; justify-content: space-between;">
@@ -67,7 +87,7 @@ export class Sun extends BasePlanet {
         `;
 
         history.innerHTML = `
-        <h2>Education / Experience</h2>
+        <h2 id="titles">Education / Experience</h2>
 
         <img id="tooling" alt="brunel" src="/assets/brunel.png" style="width: 10.8622vw; height: 7.7325vh;">
         <img id="tooling" alt="ncc" src="/assets/ncc.png" style="padding: 0 0.5vw 0 0.5vw; width: 10.8622vw; height: 7.7325vh;">
