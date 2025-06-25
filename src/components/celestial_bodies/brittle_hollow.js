76082ddd2ecf101af4d883f7e51e6a8ca03607eb
captnjayce <captn.dev@protonmail.com>
2025-06-25 18:15:06 +0100

    started work on skills page

--- src/components/celestial_bodies/brittle_hollow.js
+++ src/components/celestial_bodies/brittle_hollow.js
@@ -2,12 +2,38 @@ import { BasePlanet } from './planet.js';
 
 export class BrittleHollow extends BasePlanet {
     constructor(camera) {
-        super(camera, "Contact Me", 3.8, 1.5, 0x808080, 0);
+        super(camera, "Skills", 2, 1.5, 0x808080, 0);
 
         this.semiMajorAxis = 40;
         this.semiMinorAxis = 25;
         this.orbitSpeed = 0.5;
         this.angle = Math.random() * Math.PI * 2;
+    }
+
+    showPlanetInfo() {
+        const overlay = document.getElementById('skillsOverlay');
+        const title = overlay.querySelector('.skillsTitle');
+        const programming = overlay.querySelector('.skillsProgramming');
+        const environment = overlay.querySelector('.skillsEnvironment');
+        const other = overlay.querySelector('.skillsOther');
+
+        title.innerHTML = `
+            <h3>My Skills</h3>
+        `;
+
+        programming.innerHTML = `
+            <h3>Programming</h3>
+        `;
+
+        environment.innerHTML = `
+            <h3>Environment</h3>
+        `;
+
+        other.innerHTML = `
+            <h3>Other</h3>
+        `;
+
+        overlay.classList.add('visible');
     }
 
     update() {
