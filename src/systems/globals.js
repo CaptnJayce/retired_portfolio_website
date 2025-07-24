9fbe9bd2d48454861c5444bed6317272c6ea07f3
captnjayce <captn.dev@protonmail.com>
2025-07-24 17:41:24 +0100

    CHANGE: moved type effect functions into globals.js

---
+++ src/systems/globals.js
@@ -0,0 +1,33 @@
+export function typeEffect(element, text, speed) {
+    let i = 0;
+
+    function typing() {
+        if (i < text.length) {
+            element.innerHTML = text.substring(0, i+1) + '';
+            i++;
+            setTimeout(typing, speed);
+        } else {
+            element.innerHTML = text;
+        }
+    }
+
+    typing();
+}
+
+export function typeAsciiEffect(element, text, speed) {
+    const rows = text.split('\n');
+    let currentRow = 0;
+
+    function typing() {
+        if (currentRow < rows.length) {
+            const visibleText = rows.slice(0, currentRow + 1).join('\n');
+            element.innerHTML = visibleText + '';
+            currentRow++;
+            setTimeout(typing, speed);
+        } else {
+            element.innerHTML = text;
+        }
+    }
+
+    typing();
+}
